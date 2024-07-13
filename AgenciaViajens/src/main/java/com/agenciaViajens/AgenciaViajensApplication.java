09e67b5b7e82a13a0dae7d5ea45c22fbcf35bd1d
Imjaopedro <125218310+Imjaopedro@users.noreply.github.com>
2024-07-12 21:50:22 -0300

    Add files via upload

---
+++ AgenciaViajens/src/main/java/com/agenciaViajens/AgenciaViajensApplication.java
@@ -0,0 +1,13 @@
+package com.agenciaViajens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgenciaViajensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgenciaViajensApplication.class, args);
+	}
+
+}
---
+++ AgenciaViajens/src/test/java/com/agenciaViajens/AgenciaViajensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agenciaViajens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgenciaViajensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
